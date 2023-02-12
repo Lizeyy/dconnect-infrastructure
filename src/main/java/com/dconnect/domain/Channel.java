8db2d7218b1792574a4667434e77f107afa12e90
IWojtowicz <izawojtowicz0@gmail.com>
2023-02-12 14:45:08 +0100

    Init

---
+++ src/main/java/com/dconnect/domain/Channel.java
@@ -0,0 +1,31 @@
+package com.dconnect.infrastructureservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Channel {
+
+    @Id
+    @GeneratedValue(generator = "channel_seq")
+    @GenericGenerator(name = "channel_seq", strategy = "increment")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="server_id", nullable=false)
+    private Server serverId;
+    private String discordChannelId;
+    private String name;
+    private String creationBy;
+    private OffsetDateTime creationDate;
+    private String modificationBy;
+    private OffsetDateTime modificationDate;
+}
